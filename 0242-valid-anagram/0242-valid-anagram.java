0d7ad2fb1de1eaf1d026565d87f908e57609082f
Apoorv Jain <141646938+apoorv-jn24@users.noreply.github.com>
2026-09-10 16:39:33 +0530

    Time: 5 ms (63.05%), Space: 44.3 MB (74.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // nothing much in this question
+        // create a frequency array add character of first string anf remove the same for second string, if after all operations the array still empty(equals to 0) already then the strings are said to be anagram.
         if (s.length() != t.length()) {
             return false;
         }
